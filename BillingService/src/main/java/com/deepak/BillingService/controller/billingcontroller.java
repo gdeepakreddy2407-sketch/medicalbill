e800e0a1ff297b33b45e2745dfc24b3b9cbbbb7b
deepak <deepakreddy2019@gmail.com>
2025-11-30 12:59:05 +0530

    Add large file with LFS

--- BillingService/src/main/java/com/deepak/BillingService/controller/billingcontroller.java
+++ BillingService/src/main/java/com/deepak/BillingService/controller/billingcontroller.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class billingcontroller {
     @Autowired
     BillingService billingService;
